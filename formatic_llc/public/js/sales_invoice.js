c352bf766b6065e7fd36755e3fd134fd2662964c
Srushti238 <srushti@sanskartechnolab.com>
2024-11-13 16:45:09 +0530

    pf changes and HS code field cusotmization

---
+++ formatic_llc/public/js/sales_invoice.js
@@ -0,0 +1,14 @@
+frappe.ui.form.on('Sales Invoice Item', {
+    item_code: function(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        
+        if (row.item_code) {
+            frappe.db.get_value('Item', row.item_code, 'custom_hs_code')
+                .then(r => {
+                    if (r && r.message) {
+                        frappe.model.set_value(cdt, cdn, 'custom_hs_code', r.message.custom_hs_code);
+                    }
+                });
+        }
+    }
+});
